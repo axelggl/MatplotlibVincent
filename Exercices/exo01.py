7918fe82b146a618834be774d922173aa092fcfb
maxel <axel.maerten@ynov.com>
2024-04-25 15:09:51 +0200

    Exercice 1

---
+++ Exercices/exo01.py
@@ -0,0 +1,7 @@
+# Importez la bibliothèque Matplotlib. Utilisez également la commande magique %matplotlib inline pour permettre l'affichage des graphiques directement dans le notebook.
+
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+
